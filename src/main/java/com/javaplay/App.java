1d1184e5e3208847028d9b54ba00a8a6f1b95b7d
Kanhaiya Gawade <69666329+thisiskanhaiya@users.noreply.github.com>
2026-04-03 10:27:10 +0530

    initial commit try

---
+++ src/main/java/com/javaplay/App.java
@@ -0,0 +1,25 @@
+package com.javaplay;
+
+import java.nio.file.Paths;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+         try (Playwright playwright = Playwright.create()) {
+      Browser browser = playwright.webkit().launch();
+      Page page = browser.newPage();
+      page.navigate("https://playwright.dev/");
+      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+      
+    }
+    }
+}
